4524c698c09f0b118c5435c63340b3c1fb16d170
jb2718 <jbattle2718@gmail.com>
2021-10-21 15:06:02 -0500

    finished shuffle string exercise

--- shuffleString.js
+++ shuffleString.js
@@ -1,6 +1,7 @@
 /* 
 Given a string s and an integer array indices of the same length.
-The string s will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string.
+The string s will be shuffled such that the character at the ith position moves to 
+indices[i] in the shuffled string.
 
 Return the shuffled string.
 
@@ -12,7 +13,13 @@ Return the shuffled string.
  */
 
 var restoreString = function(s, indices) {
-    
+    let stringStore = new Array(s.length)
+
+    for(var i = 0; i < s.length; i += 1) {
+        stringStore[indices[i]] = s.charAt(i)
+    }
+
+    return stringStore.join('')
 }
 
 
@@ -20,20 +27,14 @@ var restoreString = function(s, indices) {
 // Tests/Examples
 
 //-------- Happy Paths -------- 
-restoreString("codeleet", [4,5,6,7,0,2,1,3])    // returns "leetcode"
-restoreString("abc", [0,1,2])                   // returns "abc"
-restoreString("aiohn", [3,1,4,2,0])             // returns "nihao"
-restoreString("aaiougrt", [4,0,2,6,7,3,1,5])    // returns "arigatou"
-restoreString("art", [1,0,2])                   // returns "rat"
+console.log(restoreString("codeleet", [4,5,6,7,0,2,1,3]))    // returns "leetcode"
+console.log(restoreString("abc", [0,1,2]))                   // returns "abc"
+console.log(restoreString("aiohn", [3,1,4,2,0]))             // returns "nihao"
+console.log(restoreString("aaiougrt", [4,0,2,6,7,3,1,5]))    // returns "arigatou"
+console.log(restoreString("art", [1,0,2]))                   // returns "rat"
 
 //-------- Edge Cases -------- 
 //Empty String
-restoreString("", [])                   // returns ""
+console.log(restoreString("", []))                   // returns ""
 //One character
-restoreString("a", [0])                   // returns "a"
-
-//-------- Invalid Input -------- 
-// Non-string value for first parameter
-restoreString(2, [0])                   // returns ""
-// Repeated integer values in array
-restoreString("abc", [0, 1, 1])                   // returns ""
+console.log(restoreString("a", [0]))                   // returns "a"
